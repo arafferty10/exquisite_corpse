25ad9ad12f9b159eefa4b2aa759c5fc0344a16dd
aidan <32107433+arafferty10@users.noreply.github.com>
2018-02-06 23:33:48 -0700

    simple javascript, mo story, mo CSS

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Exquisite Corpse
+// Creative Web Development
+// Spring 2017
+// Aidan Rafferty
+// Kenzy O'Neill
+// Ashley Koett
+// Anthony
+
+// https://github.com/jlmakes/scrollreveal
+window.sr = ScrollReveal({});
+sr.reveal('.fade', {duration:2500});
+
+
